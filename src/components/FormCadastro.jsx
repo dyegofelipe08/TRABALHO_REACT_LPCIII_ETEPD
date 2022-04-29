c0f0ab3fb097c53a25754f81249bae76f93c28ba
Dyego Felipe de Oliveira <dyegooliveira08@gmail.com>
2022-04-29 16:24:05 -0300

    Adicionando state no formulário de cadastro, criando classe produtoService

--- src/components/FormCadastro.jsx
+++ src/components/FormCadastro.jsx
@@ -1,44 +1,91 @@
 import React from 'react'
 import CamposObrigatorios from './CamposObrigatorios';
 
-const FormCadastro = () => {
-    return (
-        <div className='container my-4'>
+ export default class FormCadastro extends React.Component{
+
+    state = {
+        nome:'',
+        codigo:'',
+        descricao: '',
+        preco: 0,
+        fornecedor: '',
+    }
+    onChange = (event)=>{
+        const valor = event.target.value
+        const nomeDOCampo = event.target.name
+        this.setState({
+            [nomeDOCampo]:valor
+        })
+    }
+    onSubmite = (event)=>{
+        console.log(this.state)
+    }
+
+    render(){
+        return(
+<div className='container my-4'>
             <h1 className='text-center my-4'>Cadastro de Produtos</h1>
         <form>
             <div className="form-row">
                 <div className="form-group col-md-6">
                     <label htmlFor="nome">Nome: *</label>
-                    <input type="text" className="form-control" id="nome" placeholder="Nome" />
+                    <input type="text" 
+                    className="form-control" 
+                    id="nome" name='nome' 
+                    placeholder="Nome" 
+                    value={this.state.nome}
+                    onChange={this.onChange} />
                 </div>
                 <div className="form-group col-md-6">
                     <label htmlFor="codigo">Código: *</label>
-                    <input type="text" className="form-control" id="codigo" placeholder="Código" />
+                    <input type="text" 
+                    className="form-control" 
+                    id="codigo" 
+                    name='codigo' 
+                    placeholder="Código" 
+                    value={this.state.codigo}
+                    onChange={this.onChange} />
                 </div>
             </div>
             <div className="form-group">
                 <label htmlFor="descricao">Descrição: *</label>
-                <textarea type="text" className="form-control" id="descricao" placeholder="Descrição do produto..." />
+                <textarea type="text" 
+                className="form-control" 
+                id="descricao" name='descricao' 
+                placeholder="Descrição do produto..." 
+                value={this.state.descricao}
+                onChange={this.onChange} 
+                />
             </div>
             <div className="form-group">
                 <label htmlFor="preco">Preço: *</label>
-                <input type="text" className="form-control" id="preco" placeholder="R$" />
+                <input type="text" 
+                className="form-control" 
+                id="preco" name='preco' 
+                placeholder="R$" 
+                value={this.state.preco} 
+                onChange={this.onChange}/>
             </div>
             <div className="form-row">
                 <div className="form-group col-md-6">
                     <label htmlFor="fornecedor">Fornecedor: *</label>
-                    <input type="text" className="form-control" id="fornecedor" />
+                    <input type="text" 
+                    className="form-control" 
+                    id="fornecedor" 
+                    name='fornecedor' 
+                    value={this.state.fornecedor} 
+                    onChange={this.onChange}/>
                 </div>
             </div>
-           
+        
             <div className='botaoPosition m-4'>
-            <button type="submit" className="btn btn-primary">ENVIAR</button>
+            <button onClick={this.onSubmite} type='button' className="btn btn-primary">ENVIAR</button>
             </div>
         </form>
         <CamposObrigatorios/>
        
     </div>
-    );
+        );
+    }
 }
-
-export default FormCadastro;
+        
